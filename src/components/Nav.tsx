7b59a563c5c0835e54734558647d1f83e9ff992c
Flynn Longley <flynn.longley@gmail.com>
2025-05-13 17:12:44 +1200

    added a basic hamburger icon to nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,6 +1,14 @@
 import { Link } from "react-router";
+import { useState } from "react";
+import Hamburger from "@/components/ui/hamburger";
 
 function Nav() {
+
+  const [isHamburgerOpen, setHamburgerOpen] = useState(false);
+
+  const navLinks = [
+    { name: "Sign in", href: "/auth/signin" },
+  ];
 
   return (
     <>
@@ -22,54 +30,18 @@ function Nav() {
             </div>
             <div className="flex items-center gap-x-5 md:gap-x-8">
               <div className="hidden sm:block">
-                <Link
-                  className="inline-block rounded-full px-3 py-2 text-sm text-slate-700 hover:bg-slate-100 hover:text-slate-900"
-                  to="/auth/signin">
-                  Sign in
-                </Link>
+                { navLinks.map((link) => (
+                  <Link
+                    key={link.name}
+                    to={link.href}
+                    className="inline-block rounded-full px-3 py-2 text-sm text-slate-700 bg hover:bg-slate-100 hover:text-slate-900"
+                  >
+                    {link.name}
+                  </Link>
+                ))}
               </div>
               <div className="-mr-1 sm:hidden">
-                <div data-headlessui-state="">
-                  <button
-                    className="relative z-10 flex h-8 w-8 items-center justify-center focus:not-data-focus:outline-hidden"
-                    aria-label="Toggle Navigation"
-                    type="button"
-                    aria-expanded="false"
-                    data-headlessui-state=""
-                    id="headlessui-popover-button-:R5v6fja:"
-                  >
-                    <svg
-                      aria-hidden="true"
-                      className="h-3.5 w-3.5 overflow-visible stroke-slate-700"
-                      fill="none"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                    >
-                      <path
-                        d="M0 1H14M0 7H14M0 13H14"
-                        className="origin-center transition"
-                      ></path>
-                      <path
-                        d="M2 2L12 12M12 2L2 12"
-                        className="origin-center transition scale-90 opacity-0"
-                      ></path>
-                    </svg>
-                  </button>
-                </div>
-                <div style={{
-                  position: 'fixed',
-                  top: '1px',
-                  left: '1px',
-                  width: '1px',
-                  height: '0',
-                  padding: '0',
-                  margin: '-1px',
-                  overflow: 'hidden',
-                  clip: 'rect(0, 0, 0, 0)',
-                  whiteSpace: 'nowrap',
-                  borderWidth: '0',
-                  display: 'none'
-                }}></div>
+                <Hamburger toggled={isHamburgerOpen} toggle={setHamburgerOpen} />
               </div>
             </div>
           </nav>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ function Home() {
     return (
       <>
         <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-10 pb-16 text-center lg:pt-22">
-          <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">invoicing made simple</h1>
+          <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">invoicing made simple<span className="text-slate-100">r</span></h1>
         </div>
         <section className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 pt-20 pb-16 text-center lg:pt-32">
           <p>Paragraph 1</p>
